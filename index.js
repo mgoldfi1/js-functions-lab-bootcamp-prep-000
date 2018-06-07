fc8dfa5843c3ca1b42c10104d1914bbc8e013dc7
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-07 17:18:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // write your code below!
 var greeting = "Happy holidays!"
-function happyHolidays() {return greeting}
+function happyHolidays() {return greeting
+}
+function happyHolidaysTo(name) {return "Happy holidays, ${name}!"
+  }
+function happyHolidayTo
